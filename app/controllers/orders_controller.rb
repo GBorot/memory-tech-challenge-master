8ae783c2043682e37504febdb348e7411a306d56
Gautier Borot <g.borot@gmail.com>
2019-11-18 14:39:30 +0100

    i can filter by country in the DB

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,23 @@
 class OrdersController < ApplicationController
-
   def index
     if params[:query].present?
-      @orders = Order.search_by_brand_name_sport_category(params[:query])
+      @orders = Order.search_by_date_country_quantity_unit_price(params[:query])
     else
       @orders = Order.all
     end
   end
 
-  def countries
-    countries = []
-    @orders.each do |h|
-      if !countries.include?(h[:country])
-        countries << h[:country]
-    end
-  end
+  # def countries
+  #   countries = []
+  #   @orders.each do |h|
+  #     if !countries.include?(h[:country])
+  #       countries << h[:country]
 
-  def search_country
-    @orders = Order.pluck(:country).uniq
-  end
+  #   end
+  # end
 
-  def search_result
-    country = params[:query].split
-    @search_result = Order.select do |order|
-      country.include?(order.country)
-    end
-    redirect_to root_path
+  def search_by_country(country)
+    @search_result = Order.where(country: country)
   end
 
   private
